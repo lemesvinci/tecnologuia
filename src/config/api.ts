04aa9c7c53a588a934b949dec4b999911adcf7c2
lemesvinci <glemesandrade7@gmail.com>
2025-06-08 15:03:55 -0400

    atualizando

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "https://tecnologuia.onrender.com";
+export const API_URL = process.env.REACT_APP_API_URL || "https://tecnologuia.onrender.com/api";
 console.log("API_URL configurada no frontend:", API_URL);
